f86e365b68694602feccb7d78c56473d97c63119
vishnu <vishnu@NTP-LAP-767.chn.nestgroup.net>
2022-11-25 16:46:24 +0530

    add routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AddProductComponent } from './add-product/add-product.component';
 import { ViewProductComponent } from './view-product/view-product.component';
+import { Route, RouterModule, Routes } from '@angular/router';
+
+const myRoute:Routes=[
+  {
+    path:"",
+    component:AddProductComponent
+  },
+  {
+    path:"viewproduct",
+    component:ViewProductComponent
+  }
+]
 
 @NgModule({
   declarations: [
@@ -14,7 +26,8 @@ import { ViewProductComponent } from './view-product/view-product.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(myRoute)
   ],
   providers: [],
   bootstrap: [AppComponent]
